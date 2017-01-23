1d7f6c18f2fc3e3d377f783bdd320be09f7c4970
Motoharu SOMEYA <someya@ibc948.com>
2017-01-23 15:00:39 +0900

    commit

--- src/main/java/com/maretol/App.java
+++ src/main/java/com/maretol/App.java
@@ -1,13 +1,17 @@
 package com.maretol;
 
+import com.maretol.database.DBInfo;
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App{
+    public static void main(String[] args){
+        DBInfo.init();
+
+        
+
     }
 }
